bb16ad8a3d6a6c5e9f2831d9ee107fcd8a34517f
Esteban Lorenzano <estebanlm@netc.eu>
2026-02-06 12:05:37 +0100

    add test step and make test and nightly-builds build on latest P14 image

--- .ci/scripts/buildenv.py
+++ .ci/scripts/buildenv.py
@@ -1,6 +1,5 @@
-
-# the current Pharo build version to use (None for latest) 
-BUILD_IMAGE_VERSION = "482"
+# the current 'stable' (verified) Pharo build version to use 
+RELEASE_IMAGE_VERSION = "482"
 
 DEST_DIR = "dist"
 DEST_IMAGE_NAME = "Pulsar"
